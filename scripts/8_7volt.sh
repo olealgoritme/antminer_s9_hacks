7e4432aa7e6ab9facadbfc9d1f1203f088a70fd8
Ole Algoritme <olealgoritme@gmail.com>
2019-01-16 19:09:49 +0100

    added simple scripts for changing voltage

---
+++ scripts/8_7volt.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+/etc/init.d/bmminer stop
+mv /usr/bin/bmminer /usr/bin/bmminer.old
+curl --insecure -L https://github.com/olealgoritme/antminer_s9_hacks/raw/master/bmminer870 -o /usr/bin/bmminer
+chmod +x /usr/bin/bmminer
+/etc/init.d/bmminer start
